3cf3b4fd4747682a417dffb877682aabdf4f27cc
EE192-sp14 <ee192-bg@204-4.eecs.berkeley.edu>
2014-03-18 19:05:58 -0700

    stuff

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@
 #define TURN_FORWARD 1500
 #define TURN_MAXTURN 300
 
-#define NUM_SCANS 13
-#define LINE_SCANNER_SCALE 1.0f/128.0f
+#define NUM_SCANS 5
+#define LINE_SCANNER_SCALE 0.075
 #define PI 3.14159
 
 
 using namespace std;
 
-static const float eps = 1e-8;
+static const float eps = 1e-3;
 
 
 Serial pc(USBTX,USBRX);
@@ -31,12 +31,17 @@ PwmOut cam_ck(PTC9);
 DigitalOut cam_si(PTD0);
 AnalogIn cam_ao(PTB0);
 
+static float gaussian_kernel[3][5] = { { 0.0370   , 0.0720  ,  0.0899  ,  0.0720   , 0.0370},
+   { 0.0462  ,  0.0899  ,  0.1123 ,   0.0899  ,  0.0462 },
+   { 0.0370  ,  0.0720 ,   0.0899  ,  0.0720 ,   0.0370 },};
+
+static float gaussian1d_kernel[7] = {  0.0702  ,  0.1311 ,   0.1907  ,  0.2161  ,  0.1907  ,  0.1311  ,  0.0702 };
 
 void gaussian_filter(int data[][NUM_SCANS], int output[][NUM_SCANS]){
 	// using sigma1 = 1, sigma2 = 2
 	
-	int sig1 = 1;
-	int sig2 = 2;
+	//int sig1 = 1;
+	//int sig2 = 2;
 	int i, j,k,l;
 	float convsum;
 
@@ -45,8 +50,8 @@ void gaussian_filter(int data[][NUM_SCANS], int output[][NUM_SCANS]){
 		for (i = 0; i < 128; i++){
 			convsum = 0;
 		
-			for (k = -2; k < 3; k++){
-				for (l = -4; l < 5; l++){
+			for (k = -1; k < 2; k++){
+				for (l = -2; l < 3; l++){
 					int il = i + l;
 					int jk = j + k;
 					
@@ -54,7 +59,7 @@ void gaussian_filter(int data[][NUM_SCANS], int output[][NUM_SCANS]){
 						jk += NUM_SCANS;
 						
 					}
-					else if (jk > 12) {
+					else if (jk >= NUM_SCANS) {
 						jk -= NUM_SCANS;
 						
 					}
@@ -69,11 +74,7 @@ void gaussian_filter(int data[][NUM_SCANS], int output[][NUM_SCANS]){
 					
 					//inner most 2d convolution loop
 					//printf("\t%d\t%d ", i + l, j + k);
-					convsum += 1.0f / 12.0f * exp(-0.5f * (float(k*k)  + float(l*l) / 4.0f)) * elem;
-					if (convsum < 0){
-						printf("wtf");
-						convsum = 0;
-					}
+					convsum += elem * gaussian_kernel[k][l];   ///1.0f / 12.0f * exp(-0.5f * (float(k*k)  + float(l*l) / 4.0f)) * elem;
 					
 				}
 			}
@@ -86,24 +87,92 @@ void gaussian_filter(int data[][NUM_SCANS], int output[][NUM_SCANS]){
 	return;
 
 }
+void gaussian_filter(int data[], int output[]){
+	// using sigma1 = 1, sigma2 = 2
+	
+	//int sig1 = 1;
+	//int sig2 = 2;
+	int i,l;
+	float convsum;
+
+
+	
+		for (i = 0; i < 128; i++){
+			convsum = 0;
+		
+			
+				for (l = -3; l < 4; l++){
+					int il = i + l;
+					
+					
+					
+					if (il < 0){
+						il += 128;
+					}
+					else if (il > 127){
+						il -= 128;
+					}
+					
+					float elem = data[il];
+					
+					//inner most 2d convolution loop
+					//printf("\t%d\t%d ", i + l, j + k);
+					//convsum += elem * 3 * gaussian_kernel[1][l];   ///1.0f / 12.0f * exp(-0.5f * (float(k*k)  + float(l*l) / 4.0f)) * elem;
+					convsum += elem * gaussian1d_kernel[l+3];
+				}
+			
+			output[i] = int(convsum + 0.5);
+			//printf("%d, ", output[i][j]);
+		}
+	
+
+
+	return;
+
+}
 
 void differential(int data[][NUM_SCANS], int diff[][NUM_SCANS]){
 	int i,j;
 	for (j = 0; j < NUM_SCANS; j++){
 
-		for (i = 10; i < 118; i++){
-			
+		for (i = 0; i < 128; i++){
+			if (i < 8){
+				diff[i][j] = 0;
+			} else if (i > 120){
+				diff[i][j] = 0;
+			} else {
 			// 2nd derivative filter, 2nd order approximation, then 1st to compare
-			diff[i][j] =  data[(i - 1)][j] - 2 * data[i][j] + data[(i + 1)][j];
+				diff[i][j] =  data[(i - 1)][j] - 2 * data[i][j] + data[(i + 1)][j];
+			}
 		}
 	}
-
+	return;
+}
+void differential(int data[], int diff[]){
+	int i;
+	
+		
+		for (i = 0; i < 128; i++){
+			if (i < 8){
+				diff[i] = 0;
+			} else if (i > 120){
+				diff[i] = 0;
+			} else {
+			//1st derivative - could be implemented in the filter stage
+				//diff[i] = abs(data[i] - data[i-1]);
+			// 2nd derivative filter, 2nd order approximation, then 1st to compare
+				diff[i] =  data[(i - 1)] - 2 * data[i] + data[(i + 1)];
+			}
+		}
+		//data[0] = 0;
+		//data[127] = 0;
+	return;
 }
 
-int *argmax(int data[][NUM_SCANS]){
+void argmax(int data[][NUM_SCANS], int *max){
 
 	int i, j, tmp,tmp_i;
-	int *max = new int[NUM_SCANS];
+	//int *max = new int[NUM_SCANS];
 
 	for (i = 0; i < NUM_SCANS; i++){
 		tmp = 0;
@@ -116,7 +185,26 @@ int *argmax(int data[][NUM_SCANS]){
 		}
 		max[i] = tmp_i;
 	}
-	return max;
+	return;
+}
+
+int argmax(int data[]){
+
+	int j, tmp,tmp_i;
+	//int *max = new int[NUM_SCANS];
+
+		
+		tmp = 0;
+		tmp_i = 0;
+		for (j = 2; j < 126; j++){
+			if (data[j] >= tmp){
+				tmp = data[j];
+				tmp_i = j;
+			}
+		}
+		return tmp_i;
+	
+	//return max;
 }
 
 int findmax(int data[][NUM_SCANS]){
@@ -153,41 +241,50 @@ bool equalz(float a, float b){
 	}
 }
 
-float * leastsq(int data[]){
+void leastsq(int data[], float *ret){
 	int i;
-	float r, b, m;
+	float r, b, m, tmp1, tmp2;
 	float meanx = 0;
 	float meany = 0;
-	float varx = 0, stdx =0;  
-	float vary = 0, stdy =0;
-	float sumxy = 0, sumy = 0, sumx = 0, sumyy = 0, sumxx = 0;
+	float varx = 0, stdx;  
+	float vary = 0, stdy;
+	float sumy = 0, sumyy = 0;
+	float sumxy = 0;
+	float sumx = 0;
+	float sumxx = 0;
 
-	float *ret;
-	ret = new float[2];
+	//float *ret;
+	//ret = new float[2];
 
-	for (i = 0; i < NUM_SCANS; i++){
-		meany += data[i] / float(NUM_SCANS);
+	//for (i=1; i<= NUM_SCANS; i++){ 
+		
+	//}
+	
+	for (i = 1; i < NUM_SCANS; i++){
+		meany += data[i-1] / float(NUM_SCANS);
 		meanx += i / float(NUM_SCANS);
 	}
-	for (i = 0; i < NUM_SCANS; i++){
+	for (i = 1; i < NUM_SCANS; i++){
 		varx += (meanx - i)*(meanx - i) / float(NUM_SCANS);
-		vary += (meany - data[i])*(meany - data[i]) / float(NUM_SCANS);
+		vary += (meany - data[i-1])*(meany - data[i-1]) / float(NUM_SCANS);	
+		sumy += data[i-1];
+		sumyy += data[i-1] * data[i-1];
 		sumx += i;
-		sumy += data[i];
-		sumyy += data[i] * data[i];
 		sumxx += i*i;
-		sumxy += i*data[i];
+		sumxy += i *data[i-1];
 	}
+	//pc.printf("%f\t%f\t%f\r",sumx, sumxx, sumxy);
 	stdx = sqrt(varx);
 	stdy = sqrt(vary);
-	r = (float(NUM_SCANS) * sumxy - sumx*sumy) / sqrt((float(NUM_SCANS) * sumxx - sumx*sumx)*(float(NUM_SCANS) * sumyy - sumy*sumy));
-
-	m = r*stdy / stdx;
+	tmp1 = (NUM_SCANS * sumxy - sumx*sumy);
+	tmp2 = (NUM_SCANS * sumxx - sumx*sumx)*(NUM_SCANS * sumyy - sumy*sumy);
+	
+	m = tmp1 / tmp2 * stdy / stdx;
 	b = meany - m*meanx;
 
 	ret[0] = m;
 	ret[1] = b;
-	return ret;
+	//return ret;
 }
 
 float error_dist(float *y1, float *y2){
@@ -232,11 +329,11 @@ float * _minim(int data[], float m, float b){
 		if (equalz(err, olderr))
 			break;
 		if (err > olderr){
-			loop--;
+			step_size *= .5;
 			inc = -inc;
 		}
 		else {
-			step_size *= .75;
+			loop--;
 		}
 		olderr = err;
 	}
@@ -249,11 +346,11 @@ float * _minim(int data[], float m, float b){
 		if (equalz(err, olderr))
 			break;
 		if (err > olderr){ 
-			loop--;
+				step_size *= .5;
 			inc = -inc;
 		}
 		else {
-			step_size *= .75;
+			loop--;
 		}
 		olderr = err;
 	}
@@ -290,14 +387,24 @@ float *leastabs(int data[], float m, float b){
 }
 
 float find_angle(float m, float b, float d_x){
-	float angle, alpha;
+	float angle, alpha, beta;
 
-	angle = - LINE_SCANNER_SCALE / d_x * m;
-	alpha = tan(-angle);
+	alpha = 500*LINE_SCANNER_SCALE / d_x * m;
+	beta = LINE_SCANNER_SCALE / d_x * (64 - (m*NUM_SCANS+b));
+	angle = (atan(beta)); // +alpha
 
-	return alpha/PI;
+	return angle/PI;
 }
+float find_angle(int max, float d_x, float theta){
+	float angle, alpha, beta;
 
+	//alpha = LINE_SCANNER_SCALE / d_x * m;
+	alpha = 64 - max;
+	beta = LINE_SCANNER_SCALE / d_x * alpha;
+	angle = -atan(beta); // +alpha
+
+	return fabs(alpha/96)*angle/PI;
+}
 
 
 
@@ -331,7 +438,12 @@ void passiveBrake(){
 
 // per should be percent to turn, between -1 and 1
 void turn(float per){
-	int dev = 0;
+	if (per > 1.0){
+		per = 1.0;
+	} else if (per < -1.0){
+		per = -1.0;
+	}
+	int dev;
 	if( per > 0 ){
 			dev = per*TURN_RIGHT;
 	} else{
@@ -346,81 +458,99 @@ int main() {
 		
 	initPWM();
 	
-		//int isGoingFwd = goForward(0.30f); 
-	int linescan[128][NUM_SCANS] = {};
-	int lpscan[128][NUM_SCANS] = {};
-	int diffscan[128][NUM_SCANS]= {};
 	
-	int *linepts;
-	float *lsqcoef, *ladcoef;
+	//int linescan[128][NUM_SCANS] = {};
+	//int altscan[128][NUM_SCANS] = {};
+	//int linepts[13];
+	int linescan[128] = {};
+	int altscan[128] = {};
+	int linemax;
+		
+	float lsqcoef[2], ladcoef[2];
 	float *lsline;
 	lsline = new float[NUM_SCANS];
 	float *ladline;
 	ladline = new float[NUM_SCANS];
 	float *usecoef;
-	float lserrdist, laderrdist;
+	float lserrdist, laderrdist, current_speed;
+	int i;
 	//for (int i =0; i<128; i++){
 	//		linescan[i] = 0;
 	//}
-	int linemax = 0;
-	float turnto;
+	//int linemax = 0;
+	float turnto = 0;
 	int linerow = 0;
 		//Linescan camera(cam_si,cam_ck,cam_ao);
 	while(1){
 		cam_si = 1;
-		wait(.000001); 
+		wait(.00005); 
 		cam_si = 0;
 //  first three lines will clear the buffer on the camera.
 //	not exactly necesasry if using a ticker or some thread-like processing	
 		wait(0.01); //10ms integration
 		cam_si = 1;
-		wait(.001); // serial initiation pulse
+		wait(.00005); // serial initiation pulse
 		cam_si = 0;
 		// read from analog sensor
-		for (int i = 0; i<128;i++){
+		for (i = 0; i<128;i++){
 			int aout = cam_ao.read_u16();
 			//printf("pixel %d", aout);
 			//pc.printf("hm %d \n", aout);
-			linescan[i][linerow] = aout;//.5*linescan[i] + aout;
+			linescan[i] = aout;//.5*linescan[i] + aout;
 			//if (linescan[i] >= linescan[linemax]){
 			//	linemax = i;
 			//}
-			wait(0.00005); //one period of the cameras clock
+			wait(0.000012); //quarter period of the cameras clock (dunno, it works tho)
 		}
 		linerow++;
 		linerow %= NUM_SCANS;
-		//pc.printf("hm --------------------------------------------");
+		/*pc.printf("\r");
+		for(i = 0; i<128;i+=2){
+			if(linescan[i] > .75*65000){
+				pc.printf("-");
+			} else {
+				pc.printf("_");
+			}
+		}*/
+		//turnto = linerow/13.0f;
+		//turn(turnto);
 		
-		
-		gaussian_filter(linescan,lpscan);
+		gaussian_filter(linescan,altscan);
 
-		differential(lpscan,diffscan);
+		differential(altscan,linescan);
 
-		linepts = argmax(diffscan);
-
-		lsqcoef = leastsq(linepts);
 		
-		for (int i=0;i<NUM_SCANS;i++){
-			lsline[i] = i * lsqcoef[0] + lsqcoef[1];
+		
+		/*pc.printf("\r");
+		for(i = 0; i<128;i+=2){
+			if(linescan[i] > 1000){
+				pc.printf("-");
+			} else {
+				pc.printf("_");
+			}
+		}*/
+		
+		
+		
+		linemax = argmax(linescan);
+		//for (i = 0; i< NUM_SCANS; i++){
+		//	pc.printf("%f\r",lsqcoef[0]);
+		//}
+			//linemax = 0;
+		//leastsq(linepts, lsqcoef);
+		//pc.printf("%f\r",lsqcoef[0]);
+		//ladcoef = leastabs(linepts,lsqcoef[0],lsqcoef[1]);
+		
+		current_speed = 1.0;
+		//turnto += find_angle(lsqcoef[0], lsqcoef[1], current_speed);
+		turnto += find_angle(linemax, current_speed, turnto);
+		if (turnto > 1){
+			turnto = 1;
+		} else if (turnto < -1) {
+			turnto = -1;
 		}
-		//could cut out from here on
-		lserrdist = error_dist(lsline, linepts);
+	  turn(turnto);
 		
-		ladcoef = leastabs(linepts,lsqcoef[0],lsqcoef[1]);
-		for(int i=0;i<NUM_SCANS; i++){
-			ladline[i] = i*ladcoef[0] + ladcoef[1];
-		}
-		laderrdist = error_dist(ladline,linepts);
-		
-		if(laderrdist < lserrdist){
-			usecoef = ladcoef;
-		} else {
-			usecoef = lsqcoef;
-		}
-		float current_speed = 1.0;
-		float turnto = find_angle(usecoef[0], usecoef[1], current_speed);
-		
-		turn(turnto);
 	}
 
 		/*while(1){
